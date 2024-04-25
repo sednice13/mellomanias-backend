06929fa805e3e4748dd3442b18752d8cc3e1a92f
Sednice13 <sa224pb@student.lnu.se>
2024-04-25 13:31:21 +0200

    staring on server

---
+++ src/server.js
@@ -0,0 +1,63 @@
+import express from 'express'
+import helmet from 'helmet'
+import logger from 'morgan'
+import { router } from './router/router.js'
+import { connectToDatabase } from './config/mongoose.js'
+import cors from 'cors'
+
+try {
+  await connectToDatabase()
+  const app = express()
+
+  // Set various HTTP headers to make the application little more secure.
+  app.use(helmet())
+  app.use(cors())
+  // Set up a morgan logger using the dev format for log entries.
+  app.use(logger('dev'))
+
+  // Parse requests of the content type application/json.
+  app.use(express.json())
+
+  // Register routes.
+  app.use('/', router)
+
+  // Error handler.
+  app.use(function (err, req, res, next) {
+    err.status = err.status || 500
+
+    if (req.app.get('env') !== 'development') {
+      return res
+        .status(err.status)
+        .json({
+          status: err.status,
+          message: err.message
+        })
+    }
+
+    // Development only!
+    // Only providing detailed error in development.
+    return res
+      .status(err.status)
+      .json({
+        status: err.status,
+        message: err.message,
+        cause: err.cause
+          ? {
+              status: err.cause.status,
+              message: err.cause.message,
+              stack: err.cause.stack
+            }
+          : null,
+        stack: err.stack
+      })
+  })
+
+  // Starts the HTTP server listening for connections.
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`)
+    console.log('Press Ctrl-C to terminate...')
+  })
+} catch (err) {
+  console.error(err)
+  process.exitCode = 1
+}
